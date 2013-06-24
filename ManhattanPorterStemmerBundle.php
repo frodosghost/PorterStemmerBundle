e0afec3c1567fd4b99ef51defd4f4f51c2c53e72
James Rickard <james@frodosghost.com>
2013-06-24 15:53:44 +1000

    Corrected namespaces.

--- ManhattanPorterStemmerBundle.php
+++ ManhattanPorterStemmerBundle.php
@@ -4,6 +4,6 @@ namespace Manhattan\PorterStemmerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ManhattanPorterStemmerBundle extends Bundle
+class PorterStemmerBundle extends Bundle
 {
 }
